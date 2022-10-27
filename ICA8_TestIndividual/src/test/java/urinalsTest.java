5511a3c837c910b9a063c653ed9d3d11214962a7
manjubhargavisudanagunta <112654700+manjubhargavisudanagunta@users.noreply.github.com>
2022-10-27 13:06:22 -0700

    Skeleton commit

---
+++ ICA8_TestIndividual/src/test/java/urinalsTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class urinalsTest {
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+}
